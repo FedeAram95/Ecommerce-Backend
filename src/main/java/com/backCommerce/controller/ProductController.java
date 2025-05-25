ed11a342e25db766127d128a1c5fc6b592f9114e
Federico <federico.aramburu@sociallearning.io>
2025-05-25 20:59:49 -0300

    Se limpia controller.

--- src/main/java/com/backCommerce/controller/ProductController.java
+++ src/main/java/com/backCommerce/controller/ProductController.java
@@ -31,14 +31,6 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @Operation(summary = "Obtener todos los productos", description = "Devuelve una lista de todos los productos disponibles")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista de productos obtenida exitosamente",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = Product.class),
-                            examples = @ExampleObject(value = "[{\"id\":1, \"name\":\"Laptop\"}, {\"id\":2, \"name\":\"Smartphone\"}]")
-                    ))
-    })
     @GetMapping
     public ResponseEntity<List<ProductDto>> getAll(
             @RequestParam(required = false) String keyword,
@@ -50,74 +42,30 @@ public class ProductController {
     }
 
 
-    @Operation(summary = "Obtener un producto por ID", description = "Devuelve un producto basado en su ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Producto obtenido exitosamente",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = Product.class),
-                            examples = @ExampleObject(value = "{\"id\":1, \"name\":\"Laptop\"}")
-                    )),
-            @ApiResponse(responseCode = "404", description = "Producto no encontrado")
-    })
     @GetMapping("/{productId}")
     public ResponseEntity getById(@PathVariable Long productId) {
         return productService.getProductById(productId);
     }
 
 
-
-    @Operation(summary = "Crear un producto", description = "Crea un producto nuevo")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Producto creado exitosamente",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = Product.class),
-                            examples = @ExampleObject(value = "{\"id\":1, \"name\":\"Nuevo Producto\"}")
-                    )),
-            @ApiResponse(responseCode = "400", description = "Solicitud inválida")
-    })
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ProductDto> create(
-    @RequestParam("product") String productJson,
-    @RequestParam(value = "image", required = false) MultipartFile imageFile) throws JsonProcessingException {
-        
-            // Convertir JSON a objeto ProductDto
-        ObjectMapper objectMapper = new ObjectMapper();
-        ProductDto product = objectMapper.readValue(productJson, ProductDto.class);
-        
-        return productService.createProduct(product, imageFile);
+        @RequestPart("product") ProductDto productDto,
+        @RequestPart(value = "image", required = false) MultipartFile imageFile
+    ) throws JsonProcessingException {
+        return productService.createProduct(productDto, imageFile);
     }
 
-    @Operation(summary = "Actualizar un producto por ID", description = "Actualiza un producto existente basado en su ID")
-@ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Producto actualizado exitosamente",
-                content = @Content(mediaType = "application/json",
-                        schema = @Schema(implementation = Product.class),
-                        examples = @ExampleObject(value = "{\"id\":1, \"name\":\"Producto Actualizado\"}")
-                )),
-        @ApiResponse(responseCode = "404", description = "Producto no encontrado"),
-        @ApiResponse(responseCode = "400", description = "Solicitud inválida")
-})
-@PutMapping(value = "/{productId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-public ResponseEntity<ProductDto> updateProduct(
-    @PathVariable Long productId,
-    @RequestParam("product") String productJson, // Recibe el JSON como String
-    @RequestParam(value = "image", required = false) MultipartFile imageFile) throws JsonProcessingException {
 
-    // Convertir JSON a objeto ProductDto
-    ObjectMapper objectMapper = new ObjectMapper();
-    ProductDto product = objectMapper.readValue(productJson, ProductDto.class);
+	@PutMapping(value = "/{productId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<ProductDto> updateProduct(
+	    @PathVariable Long productId,
+        @RequestPart("product") ProductDto productDto,
+        @RequestPart(value = "image", required = false) MultipartFile imageFile) throws JsonProcessingException {	
+	    return productService.updateProduct(productId, productDto, imageFile);
+	}
+    
 
-    return productService.updateProduct(productId, product, imageFile);
-}
-    @Operation(summary = "Eliminar un producto por ID", description = "Elimina un producto existente basado en su ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Producto eliminado exitosamente",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = Product.class),
-                            examples = @ExampleObject(value = "{\"id\":1, \"name\":\"Laptop\"}")
-                    )),
-            @ApiResponse(responseCode = "404", description = "Producto no encontrado")
-    })
     @DeleteMapping("/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long productId) {
         return productService.deleteProduct(productId);
